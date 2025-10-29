3d9083e04c63ac7790d4a0abe2cff2405896c028
Jhon Paez <paezgarayjhonfreddy@gmail.com>
2025-10-28 20:30:09 -0500

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ import ContactForm from "./components/ContactForm";
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-start p-6">
-      <Navbar />
-      <Header />
-      <Programs />
-      <ContactForm />
+      <Navbar/>
+      <Header/>
+      <Programs/>
+      <ContactForm/>
     </div>
   );
 }
